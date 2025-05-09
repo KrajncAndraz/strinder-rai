cabb39d18c62eafa217e78014166295462ffe2e0
branko-bit <brankomajsen@gmail.com>
2025-05-09 15:24:22 +0200

    test push

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+//test
 // vključimo mongoose in ga povežemo z MongoDB
 var mongoose = require('mongoose');
 var mongoDB = "mongodb://127.0.0.1/strinder";
