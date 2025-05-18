15127991b74be85213546ae7985701202bdc9865
KrajncAndraz <andraz.krajnc1@student.um.si>
2025-05-18 14:36:32 +0200

    Improved Profile menu visuals

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../userContext';
 import { Navigate } from 'react-router-dom';
+import '../styles/Profile.css';
 
 function Profile(){
     const userContext = useContext(UserContext); 
@@ -17,10 +18,14 @@ function Profile(){
 
     return (
         <>
-            {!userContext.user ? <Navigate replace to="/login" /> : ""}
-            <h1>User profile</h1>
-            <p>Username: {profile.username}</p>
-            <p>Email: {profile.email}</p>
+            <div className="profile">
+                {!userContext.user ? <Navigate replace to="/login" /> : ""}
+                <h1>User profile</h1>
+                <img src="Profile-Default.svg" alt="Profile"></img>
+                <p>{profile.username}</p>
+                <p>{profile.email}</p>
+                <button>Edit Profile</button>
+            </div>
         </>
     );
 }
