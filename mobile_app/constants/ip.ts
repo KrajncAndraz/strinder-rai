eb773f0078d82cd91007be688cc6a8bb8925d5a8
branko-bit <brankomajsen@gmail.com>
2025-05-27 13:22:24 +0200

    spremenil gitignore

--- mobile_app/constants/ip.ts
+++ mobile_app/constants/ip.ts
@@ -1,6 +1,6 @@
 //LIAM IP
-export const BASE_URL = 'http://10.0.2.2:3001';
+//export const BASE_URL = 'http://10.0.2.2:3001';
 //BRANKO IP
-// export const BASE_URL = 'http://
+ export const BASE_URL = 'http://10.20.10.5:3001';
 //ANDRAŽ IP
 // export const BASE_URL = 'http://
