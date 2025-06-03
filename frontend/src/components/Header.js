0a23ce3e16c182abdb753ac144426916a1ef52c7
KrajncAndraz <andraz.krajnc1@student.um.si>
2025-06-03 18:12:08 +0200

    STA-37 Update header layout based on logged in status

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,33 +4,31 @@ import { Link } from "react-router-dom";
 import '../styles/Header.css';
 
 function Header(props) {
+    const context = useContext(UserContext);
+    const isLoggedIn = !!context.user;
+
     return (
         <header>
-            <div className="header-container">
+            <div className={`header-container ${isLoggedIn ? "header-space-around" : "header-left-gap"}`}>
                 <Link to='/'>Home</Link>
-                <UserContext.Consumer>
-                    {context => (
-                        context.user ?
-                            <>
-                                <Link to='/profile'>Profile</Link>
-                                <Link to='/friends'>Friends</Link>
-                                <Link to='/addFriend'>Add Friend</Link>
-                                <Link to='/friendRequests'>Friend Requests</Link>
-                                <Link to='/chatlogs'>Chat logs</Link>
-                                <Link to='/workouts'>Workouts</Link>
-                                <Link to='/mqtt'>Mqtt</Link>
-                                <Link to='/statistics'>Statistics</Link>
-                                <Link to='/logout'>Logout</Link>
-
-                            </>
-                            :
-                            <>
-                                <Link to='/login'>Login</Link>
-                                <Link to='/register'>Register</Link>
-                            </>
-
-                    )}
-                </UserContext.Consumer>
+                {isLoggedIn ? (
+                    <>
+                        <Link to='/profile'>Profile</Link>
+                        <Link to='/friends'>Friends</Link>
+                        <Link to='/addFriend'>Add Friend</Link>
+                        <Link to='/friendRequests'>Friend Requests</Link>
+                        <Link to='/chatlogs'>Chat logs</Link>
+                        <Link to='/workouts'>Workouts</Link>
+                        <Link to='/mqtt'>Mqtt</Link>
+                        <Link to='/statistics'>Statistics</Link>
+                        <Link to='/logout'>Logout</Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to='/login'>Login</Link>
+                        <Link to='/register'>Register</Link>
+                    </>
+                )}
             </div>
         </header>
     );
